8506dccea21729ecce053af828e080854b3f45ee
fahim2k25 <fahim.program@gmail.com>
2025-04-18 23:31:54 +0600

    created my first component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ function App() {
   return (
     <>
       <h1>React Fundamentals</h1>
+      <Person></Person>
+      <Person></Person>
+    </>
+  )
+}
+
+function Person() {
+
+  return (
+    <>
+      <h3>Person Details</h3>
+      <ul>
+        <li>Name: </li>
+        <li>Age: </li>
+        <li>Profession: </li>
+      </ul>
     </>
   )
 }
