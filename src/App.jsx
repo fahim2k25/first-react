ed556ccfda7e343cfdc1779442933804f57107b3
fahim2k25 <fahim.program@gmail.com>
2025-04-18 23:47:16 +0600

    added props with destructuring method in components

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ function App() {
   return (
     <>
       <h1>React Fundamentals</h1>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Ada Lovelace" age="24" job="Web developer"></Person>
+      <Person name="Cathy Wickerman" age="47" job="Data Scientist"></Person>
     </>
   )
 }
 
-function Person() {
+function Person({ name, age, job }) {
 
   return (
-    <>
-      <h3>Person Details</h3>
-      <ul>
-        <li>Name: </li>
-        <li>Age: </li>
-        <li>Profession: </li>
+    <div className="person-box">
+      <h3 className='title'>Person Details</h3>
+      <ul >
+        <li>Name: {name}</li>
+        <li>Age: {age}</li>
+        <li>Profession: {job}</li>
       </ul>
-    </>
+    </div>
   )
 }
 
